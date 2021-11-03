ece8d07db43c634c6d78b09700fdd01a04a13d8e
Paul Battley <pbattley@gmail.com>
2021-11-03 17:24:22 +0000

    Use mocks for authentication spec
    This is now tested end-to-end in the integration specs

--- spec/authenticator_spec.rb
+++ spec/authenticator_spec.rb
@@ -4,15 +4,49 @@ require "pacer/authenticator"
 
 RSpec.describe Pacer::Authenticator do
   subject(:authenticator) {
-    described_class.new(login, password, environment: :qa)
+    described_class.new("kodos2024", "Passw0rd", environment: :qa)
   }
 
-  describe "authentication", :vcr do
-    context "when unsuccessful" do
-      let(:login) { "kodos2024" }
-      let(:password) { "Incorr3ct!" }
+  let(:response_body) { '{"nextGenCSO":"a","loginResult":"0"}' }
+  let(:response_headers) { {} }
 
-      it "raises an exception" do
+  before do
+    stub_request(:post, %r{\Ahttps://qa-login.uscourts.gov/})
+      .to_return(body: response_body, headers: response_headers)
+  end
+
+  describe "authentication" do
+    it "sends JSON request headers" do
+      authenticator.authenticate
+
+      expect(WebMock)
+        .to have_requested(:post, /.*/)
+        .with(headers: {
+          "Content-Type" => "application/json",
+          "Accept" => "application/json"
+        })
+    end
+
+    it "posts the required body" do
+      authenticator.authenticate
+
+      expect(WebMock)
+        .to have_requested(:post, /.*/)
+        .with { |req|
+          JSON.parse(req.body) == {
+            "loginId" => "kodos2024",
+            "password" => "Passw0rd",
+            "redactFlag" => "1"
+          }
+        }
+    end
+
+    context "when unsuccessful" do
+      let(:response_body) {
+        '{"loginResult":"1","errorDescription":"Login Failed"}'
+      }
+
+      it "raises an exception with the error description" do
         expect { authenticator.authenticate }
           .to raise_exception(Pacer::AuthenticationError)
           .with_message("Login Failed")
@@ -20,8 +54,7 @@ RSpec.describe Pacer::Authenticator do
     end
 
     context "when successful" do
-      let(:login) { PACER_LOGIN }
-      let(:password) { PACER_PASSWORD }
+      let(:response_body) { '{"nextGenCSO":"CSOTOKEN","loginResult":"0"}' }
 
       it "returns a session object" do
         expect(authenticator.authenticate).to be_kind_of(Pacer::Session)
@@ -34,7 +67,7 @@ RSpec.describe Pacer::Authenticator do
 
         expect(Pacer::Session)
           .to have_received(:new)
-          .with(/\A[A-Za-z0-9]{128}\z/, environment: :qa)
+          .with("CSOTOKEN", environment: :qa)
       end
     end
   end
