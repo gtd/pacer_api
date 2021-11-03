7f9a04a6887b9f6266e8a8a9c6c5851d93275994
Paul Battley <pbattley@gmail.com>
2021-11-03 11:09:35 +0000

    Add debugging assistance
    - When the DEBUG environment variable is set, the request and response
      bodies are logged to standard out
    - pp is always available in specs

--- lib/pacer/translation.rb
+++ lib/pacer/translation.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "json"
+require "pp" if ENV.key?("DEBUG")
 
 require "pacer/request_translator"
 require "pacer/response_translator"
@@ -8,11 +9,20 @@ require "pacer/response_translator"
 module Pacer
   module Translation
     def encode_request(obj)
+      if ENV.key?("DEBUG")
+        puts ">>>>"
+        pp obj
+      end
       JSON.generate(RequestTranslator.translate(obj))
     end
 
     def decode_response(body)
-      ResponseTranslator.translate(JSON.parse(body))
+      ResponseTranslator.translate(JSON.parse(body)).tap { |obj|
+        if ENV.key?("DEBUG")
+          puts "<<<<"
+          pp obj
+        end
+      }
     end
   end
 end
